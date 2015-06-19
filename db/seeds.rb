e92719b0ba6e96a75643f2046bbf7b762577903f
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-19 18:03:35 -0400

    adding working associations and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+survey = Survey.create(name: "survey 1", url: "www.survey1.com")
+user = User.create(name: "john", email: "john@gmail.com", password_digest: "123")
+question = Question.create(body: "What's your fav scary movie?")
+answer = Answer.create(body: "finding nemo")
+answer2 = Answer.create(body: "cars")
+answer3 = Answer.create(body: "toy story")
+answer4 = Answer.create(body: "tales from the crypt")
+
+
+survey.questions << question
+question.answers << answer
+question.answers << answer2
+question.answers << answer3
+question.answers << answer4
+user.surveys << survey
