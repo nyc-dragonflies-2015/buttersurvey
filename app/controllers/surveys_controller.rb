1d9d6a32df08551f8c2e3205e6f8144813e8e68d
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-20 15:56:23 -0400

    start of read and create

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,7 +1,19 @@
 class SurveysController < ApplicationController
+
   def results
     @survey = Survey.find_by(params[:id])
     @questions = Survey.find_by(params[:id]).questions
   end
 
+  def index
+    @surveys = Survey.all
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(params[:survey][:name])
+  end
 end
