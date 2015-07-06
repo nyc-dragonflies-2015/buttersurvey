afe70544098a5f9e3de926e9668a5af3d2f1b626
benjamincohen1989 & Julia Castro <pair+benjamincohen1989.julia-castro@devbootcamp.com>
2015-07-06 11:17:09 -0400

    add user sign up test

--- spec/features/user_create_spec.rb
+++ spec/features/user_create_spec.rb
@@ -13,4 +13,27 @@ describe "welcome page" do
   end
 end
 
+describe "sign up" do
+  it "should create new user" do
+    visit login_path
+    within("#new_user") do
+      fill_in 'user_name', :with => 'ben'
+      fill_in 'user_email', :with => 'ben@example.com'
+      fill_in 'user_password', :with => '123'
+    end
+    click_button 'Create User'
+    expect(page).to have_content 'Account successfully created'
+  end
+end
+
+
+
+# describe "create user" do
+#   let!(:user) {User.create(name: "ben", email: "ben@example.com", password: "123") }
+#   it "should let you create a new user" do
+#   byebug
+
+#   end
+# end
+
 
